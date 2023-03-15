2c1ffa0c6e06a4ae68f10b92659fb0030df3081e
Andreas Nilsson <andreas.nilsson@hkr.se>
2023-03-15 11:04:28 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        // Loopa igenom talen 1 till 12 och skriv för varje tal ut
+        //   - Talet
+        //   - Talet i kvadrat
+        //   - Talet i kubik
+
+
+    }
+}
